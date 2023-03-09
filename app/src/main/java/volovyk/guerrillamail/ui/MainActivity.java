ccb773285b7a14ed295965b796b0c6657dac08b5
Oleksandr <olieksandr.volovik@gmail.com>
2023-03-09 14:37:33 +0200

    Initial commit

---
+++ app/src/main/java/volovyk/guerrillamail/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package volovyk.guerrillamail.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import volovyk.guerrillamail.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
