3fd2e37db69d8a2a0db078413c952aa42b85b619
Oleksandr <olieksandr.volovik@gmail.com>
2023-03-22 17:02:12 +0200

    More refactoring

--- app/src/main/java/volovyk/guerrillamail/ui/MainActivity.java
+++ app/src/main/java/volovyk/guerrillamail/ui/MainActivity.java
@@ -1,16 +1,14 @@
 package volovyk.guerrillamail.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import dagger.hilt.android.AndroidEntryPoint;
 import volovyk.guerrillamail.R;
@@ -18,26 +16,30 @@ import volovyk.guerrillamail.R;
 @AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
 
-    private MainViewModel mainViewModel;
+    private String assignedEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        MainViewModel mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
 
         TextView emailTextView = findViewById(R.id.emailTextView);
 
         mainViewModel.getAssignedEmail().observe(this,
-                s -> emailTextView.setText(getString(R.string.your_temporary_email, s)));
+                email -> {
+                    emailTextView.setText(getString(R.string.your_temporary_email, email));
+                    assignedEmail = email;
+                });
 
-        emailTextView.setOnClickListener(v -> {
-            ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-            ClipData clip = ClipData.newPlainText("Email", mainViewModel.getAssignedEmail().getValue());
-            clipboard.setPrimaryClip(clip);
+        emailTextView.setOnClickListener(v -> copyEmailToClipboard());
+    }
 
-            Toast.makeText(this, R.string.email_in_clipboard, Toast.LENGTH_SHORT).show();
-        });
+    private void copyEmailToClipboard() {
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText(getString(R.string.app_name), assignedEmail);
+        clipboard.setPrimaryClip(clip);
+        Toast.makeText(this, R.string.email_in_clipboard, Toast.LENGTH_SHORT).show();
     }
 }
