44f30b74888b0614344a1cf482a5cf74a4319aca
Oleksandr <olieksandr.volovik@gmail.com>
2023-06-26 22:14:42 +0300

    SpecificEmailFragment refactoring

--- app/src/main/java/volovyk/guerrillamail/ui/SpecificEmailFragment.kt
+++ app/src/main/java/volovyk/guerrillamail/ui/SpecificEmailFragment.kt
@@ -6,35 +6,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import volovyk.guerrillamail.R
-import volovyk.guerrillamail.data.model.Email
 import volovyk.guerrillamail.databinding.FragmentSpecificEmailBinding
 
 @AndroidEntryPoint
 class SpecificEmailFragment : Fragment() {
-    private var chosenEmail = 1
+    private var chosenEmailIndex = 1
     private var _binding: FragmentSpecificEmailBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (arguments != null) {
-            chosenEmail = requireArguments().getInt(ARG_CHOSEN_EMAIL)
-        }
+        arguments?.getInt(ARG_CHOSEN_EMAIL)?.let { chosenEmailIndex = it }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val mainViewModel = ViewModelProvider(requireActivity())[MainViewModel::class.java]
-        mainViewModel.emails.observe(viewLifecycleOwner) { emails: List<Email?>? ->
-            val chosenEmail = emails?.get(chosenEmail)
-            if (chosenEmail != null) {
-                binding.fromTextView.text = getString(R.string.from, chosenEmail.from)
-                binding.subjectTextView.text = getString(R.string.subject, chosenEmail.subject)
-                binding.dateTextView.text = getString(R.string.date, chosenEmail.date)
-                binding.bodyTextView.text = Html.fromHtml(chosenEmail.body, Html.FROM_HTML_MODE_COMPACT)
+        val mainViewModel: MainViewModel by viewModels()
+        mainViewModel.emails.observe(viewLifecycleOwner) { emails ->
+            val chosenEmail = emails?.get(chosenEmailIndex)
+            chosenEmail?.let {
+                binding.fromTextView.text = getString(R.string.from, it.from)
+                binding.subjectTextView.text = getString(R.string.subject, it.subject)
+                binding.dateTextView.text = getString(R.string.date, it.date)
+                binding.bodyTextView.text = Html.fromHtml(it.body, Html.FROM_HTML_MODE_COMPACT)
             }
         }
     }
