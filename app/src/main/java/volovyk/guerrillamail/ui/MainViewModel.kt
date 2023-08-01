7b64b76b9c01b9289d4147bdb4115a0d66cbca27
Oleksandr <olieksandr.volovik@gmail.com>
2023-08-01 17:08:06 +0300

    Use combine instead of collecting each Flow to update uiState

--- app/src/main/java/volovyk/guerrillamail/ui/MainViewModel.kt
+++ app/src/main/java/volovyk/guerrillamail/ui/MainViewModel.kt
@@ -3,12 +3,10 @@ package volovyk.guerrillamail.ui
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import volovyk.guerrillamail.data.EmailRepository
 import volovyk.guerrillamail.data.model.Email
@@ -24,9 +22,6 @@ data class UiState(
 @HiltViewModel
 class MainViewModel @Inject constructor(private val emailRepository: EmailRepository) :
     ViewModel() {
-
-    private val _uiState = MutableStateFlow(UiState())
-    val uiState: StateFlow<UiState> = _uiState.asStateFlow()
 
     private val assignedEmail =
         emailRepository.observeAssignedEmail().stateIn(
@@ -49,23 +44,14 @@ class MainViewModel @Inject constructor(private val emailRepository: EmailReposi
             RemoteEmailDatabase.State.Loading
         )
 
-    init {
-        viewModelScope.launch {
-            assignedEmail.collect { assignedEmail ->
-                _uiState.update { it.copy(assignedEmail = assignedEmail) }
-            }
-        }
-        viewModelScope.launch {
-            emails.collect { emails ->
-                _uiState.update { it.copy(emails = emails) }
-            }
-        }
-        viewModelScope.launch {
-            state.collect { state ->
-                _uiState.update { it.copy(state = state) }
-            }
-        }
-    }
+    val uiState: StateFlow<UiState> =
+        combine(assignedEmail, emails, state) { assignedEmail, emails, state ->
+            UiState(assignedEmail, emails, state)
+        }.stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000),
+            UiState()
+        )
 
     fun setEmailAddress(newAddress: String) {
         viewModelScope.launch {
