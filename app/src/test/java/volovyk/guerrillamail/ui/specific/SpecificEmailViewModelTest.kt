ce0e90ad96f56e4cd260dbbf9ad47fe76fc9d5c0
Oleksandr <olieksandr.volovik@gmail.com>
2023-09-01 20:11:28 +0300

    SpecificEmailViewModel tests

---
+++ app/src/test/java/volovyk/guerrillamail/ui/specific/SpecificEmailViewModelTest.kt
@@ -0,0 +1,64 @@
+package volovyk.guerrillamail.ui.specific
+
+import androidx.lifecycle.SavedStateHandle
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
+import volovyk.MainCoroutineRule
+import volovyk.guerrillamail.data.EmailRepository
+import volovyk.guerrillamail.data.model.Email
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class SpecificEmailViewModelTest {
+
+    private lateinit var viewModel: SpecificEmailViewModel
+    private lateinit var emailRepository: EmailRepository
+    private lateinit var savedStateHandle: SavedStateHandle
+
+    // Set the main coroutines dispatcher for unit testing.
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    val mainCoroutineRule = MainCoroutineRule()
+
+    @Before
+    fun setup() {
+        emailRepository = mock(EmailRepository::class.java)
+        savedStateHandle = SavedStateHandle()
+    }
+
+    @Test
+    fun `init with emailId sets uiState email when emailId is provided`() = runTest {
+        // Given
+        val emailId = 123
+        val email = Email(0, "", "", "", "", false)
+
+        savedStateHandle["emailId"] = emailId
+        `when`(emailRepository.getEmailById(emailId)).thenReturn(email)
+
+        // When
+        viewModel = SpecificEmailViewModel(savedStateHandle, emailRepository)
+
+        // Then
+        val uiState = viewModel.uiState.value
+        assertEquals(email, uiState.email)
+    }
+
+    @Test
+    fun `init without emailId does not set uiState email`() = runTest {
+        // Given
+        savedStateHandle.remove<Int>("emailId")
+
+        // When
+        viewModel = SpecificEmailViewModel(savedStateHandle, emailRepository)
+
+        // Then
+        val uiState = viewModel.uiState.value
+        assertNull(uiState.email)
+    }
+}
