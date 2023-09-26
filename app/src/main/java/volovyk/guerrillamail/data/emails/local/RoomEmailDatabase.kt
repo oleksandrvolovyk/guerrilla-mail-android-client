c1dea7f451fa3e03a67da7ce5eaab7a8b5570d49
Oleksandr <olieksandr.volovik@gmail.com>
2023-09-26 07:59:16 +0300

    fix: update Room Database version number

--- app/src/main/java/volovyk/guerrillamail/data/emails/local/RoomEmailDatabase.kt
+++ app/src/main/java/volovyk/guerrillamail/data/emails/local/RoomEmailDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import volovyk.guerrillamail.data.emails.model.Email
 
-@Database(entities = [Email::class], version = 4)
+@Database(entities = [Email::class], version = 5)
 abstract class RoomEmailDatabase : RoomDatabase(), LocalEmailDatabase {
     abstract override fun getEmailDao(): EmailDao
 }
