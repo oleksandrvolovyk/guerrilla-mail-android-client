10812224c09d3dbca0dffb1695687c368b71e40c
Oleksandr <olieksandr.volovik@gmail.com>
2023-10-27 18:29:05 +0300

    fix: Use "withContext" to ensure that "setValue" and "getValue" execute on the IO dispatcher

--- app/src/main/java/volovyk/guerrillamail/data/preferences/PreferencesRepositoryImpl.kt
+++ app/src/main/java/volovyk/guerrillamail/data/preferences/PreferencesRepositoryImpl.kt
@@ -1,15 +1,17 @@
 package volovyk.guerrillamail.data.preferences
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import volovyk.guerrillamail.data.preferences.local.PreferencesDatasource
 
 class PreferencesRepositoryImpl(private val preferencesDatasource: PreferencesDatasource) :
     PreferencesRepository {
 
-    override suspend fun setValue(key: String, value: String) {
+    override suspend fun setValue(key: String, value: String) = withContext(Dispatchers.IO) {
         preferencesDatasource.setValue(key, value)
     }
 
-    override suspend fun getValue(key: String): String? {
-        return preferencesDatasource.getValue(key)
+    override suspend fun getValue(key: String): String? = withContext(Dispatchers.IO) {
+        preferencesDatasource.getValue(key)
     }
 }
