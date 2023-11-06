ceba9502228e762946e589ac3d48856066a9f141
Oleksandr <olieksandr.volovik@gmail.com>
2023-11-06 14:51:16 +0200

    feature: Remove ads

--- app/src/main/java/volovyk/guerrillamail/ui/specific/SpecificEmailFragment.kt
+++ app/src/main/java/volovyk/guerrillamail/ui/specific/SpecificEmailFragment.kt
@@ -1,46 +1,22 @@
 package volovyk.guerrillamail.ui.specific
 
-import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.widget.CompoundButton
-import androidx.activity.OnBackPressedCallback
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 import volovyk.guerrillamail.R
-import volovyk.guerrillamail.data.ads.Ad
-import volovyk.guerrillamail.data.ads.AdManager
 import volovyk.guerrillamail.data.emails.model.Email
 import volovyk.guerrillamail.databinding.FragmentSpecificEmailBinding
 import volovyk.guerrillamail.ui.BaseFragment
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class SpecificEmailFragment :
     BaseFragment<FragmentSpecificEmailBinding>(FragmentSpecificEmailBinding::inflate) {
 
     private val viewModel: SpecificEmailViewModel by viewModels()
-
-    @Inject
-    lateinit var adManager: AdManager
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        adManager.loadAd(Ad.Interstitial)
-        val callback = object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                activity?.let { adManager.tryToShowAd(it, Ad.Interstitial) }
-                this.remove()
-                activity?.onBackPressedDispatcher?.onBackPressed()
-            }
-        }
-        requireActivity().onBackPressedDispatcher.addCallback(
-            owner = this,
-            onBackPressedCallback = callback
-        )
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         Timber.d("onViewCreated")
