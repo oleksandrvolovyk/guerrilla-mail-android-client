aba5738c65ea5037b2d0635f49b72a99fc500ceb
Oleksandr <olieksandr.volovik@gmail.com>
2023-12-07 22:48:45 +0200

    refactor: Remove unused code

--- app/src/main/java/volovyk/guerrillamail/ui/UiHelper.kt
+++ app/src/main/java/volovyk/guerrillamail/ui/UiHelper.kt
@@ -2,8 +2,6 @@ package volovyk.guerrillamail.ui
 
 import android.content.Context
 import android.content.DialogInterface
-import android.text.Editable
-import android.text.TextWatcher
 import androidx.appcompat.app.AlertDialog
 import volovyk.guerrillamail.R
 
@@ -25,11 +23,5 @@ object UiHelper {
             dialog.dismiss()
         }
         return builder.create()
-    }
-
-    abstract class SimpleTextWatcher : TextWatcher {
-        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
-        override fun afterTextChanged(s: Editable) {}
     }
 }
