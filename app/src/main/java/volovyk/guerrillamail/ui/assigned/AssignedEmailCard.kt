57618508e3925c954c8146fad9d5851255f9a3a6
Oleksandr <olieksandr.volovik@gmail.com>
2024-04-17 15:47:17 +0300

    feature: "Copy" icon to make the ability to copy an email address more intuitive

--- app/src/main/java/volovyk/guerrillamail/ui/assigned/AssignedEmailCard.kt
+++ app/src/main/java/volovyk/guerrillamail/ui/assigned/AssignedEmailCard.kt
@@ -6,15 +6,23 @@ import androidx.compose.animation.expandVertically
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,20 +44,34 @@ fun AssignedEmailCard(
     Card(
         modifier = modifier
     ) {
-        Text(
-            text = if (emailUsername == null) {
-                context.getString(R.string.getting_temporary_email)
-            } else {
-                context.getString(R.string.your_temporary_email)
-            },
+        Row(
             modifier = Modifier
                 .clickable(onClick = onEmailAddressClick)
                 .fillMaxWidth()
                 .background(MaterialTheme.colorScheme.primary)
                 .padding(8.dp),
-            textAlign = TextAlign.Center,
-            color = MaterialTheme.colorScheme.onPrimary
-        )
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = if (emailUsername == null) {
+                    context.getString(R.string.getting_temporary_email)
+                } else {
+                    context.getString(R.string.your_temporary_email)
+                },
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+
+            if (emailUsername != null) {
+                Spacer(Modifier.width(8.dp))
+                Icon(
+                    painter = painterResource(R.drawable.ic_copy),
+                    contentDescription = stringResource(R.string.copy_address_to_clipboard),
+                    tint = MaterialTheme.colorScheme.onPrimary
+                )
+            }
+        }
 
         AnimatedVisibility(
             visible = (emailUsername != null && emailDomain != null),
