f8bccd28696aec55789e4204ef724a2c015b4dd0
Oleksandr <olieksandr.volovik@gmail.com>
2024-04-26 15:15:42 +0300

    refactor: Add StringRes annotations

--- app/src/main/java/volovyk/guerrillamail/ui/SideEffect.kt
+++ app/src/main/java/volovyk/guerrillamail/ui/SideEffect.kt
@@ -4,15 +4,23 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import androidx.activity.ComponentActivity
+import androidx.annotation.StringRes
 import volovyk.guerrillamail.R
 import volovyk.guerrillamail.ui.UiHelper.createConfirmationDialog
 import volovyk.guerrillamail.ui.UiHelper.showToast
 
 sealed interface SideEffect {
-    data class ShowToast(val stringId: Int, val stringFormatArg: String? = null) : SideEffect
-    data class CopyTextToClipboard(val text: String) : SideEffect
+    data class ShowToast(
+        @StringRes val stringId: Int,
+        val stringFormatArg: String? = null
+    ) : SideEffect
+
+    data class CopyTextToClipboard(
+        val text: String
+    ) : SideEffect
+
     data class ConfirmAction(
-        val messageStringId: Int,
+        @StringRes val messageStringId: Int,
         val stringFormatArg: String? = null,
         val action: () -> Unit
     ) : SideEffect
