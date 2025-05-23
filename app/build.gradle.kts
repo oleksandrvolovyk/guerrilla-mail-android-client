5ead059f3671966fc221a51480d7961968f8b6e7
Oleksandr Volovyk <oleksandr.volovyk@trident-media.agency>
2025-05-23 15:19:44 +0300

    Version 46 (3.5.8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,8 +49,8 @@ android {
         applicationId = "volovyk.guerrillamail"
         minSdk = 24
         targetSdk = 34
-        versionCode = 45
-        versionName = "3.5.7"
+        versionCode = 46
+        versionName = "3.5.8"
 
         testInstrumentationRunner = "volovyk.MyTestRunner"
 
